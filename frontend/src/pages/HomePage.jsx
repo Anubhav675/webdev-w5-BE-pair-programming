bbab5f1782151bd5c77ee1367698131723241ab5
Yamong Phoo <yamongphoo012@gmail.com>
2026-09-16 14:47:47 +0300

    [iter2] Implement job fetching on homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,28 @@ import { useEffect, useState } from "react";
 const Home = () => {
   const [jobs, setJobs] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("/api/jobs");
+        const data = await res.json();
+        console.log(data);
+
+        setJobs(data)
+
+
+
+      }
+
+      catch (error) {
+        console.error("fail to fetch jobs: ", error);
+      }
+    };
+
+    fetchData();
+    console.log(jobs);
+  });
+
   return (
     <div className="home">
       <div className="job-list">
@@ -14,6 +36,8 @@ const Home = () => {
     </div>
   );
 };
+
+
 
 export default Home;
 
